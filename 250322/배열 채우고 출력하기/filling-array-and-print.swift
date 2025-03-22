6b9f570fd4dd43ba0a7667d26e98d2c3442ba3f8
GthingkingG <21thingking@gmail.com>
2025-03-22 14:30:31 +0900

    Title: 배열 채우고 출력하기, Time: 11ms, Memory: 1MB, Status: Passed - CodeTree

--- 250322/배열 채우고 출력하기/filling-array-and-print.swift
+++ 250322/배열 채우고 출력하기/filling-array-and-print.swift
@@ -1,17 +1,5 @@
-var array: [String] = []
-if let input = readLine() {
- let letters = input.split(separator: " ").compactMap { $0 }
- if letters.count == 10 {
-    for i: Int in 1...10 {
-        // array[i] = letters[i]
-        print(letters[i])
-    }
-    for j in letters.count...1 {
-        print("\(array[j])")
-    }
- } else {
- print("정확히 10개의 숫자를 입력해주세요.")
+let characters = readLine()!.split(separator: " ")
+for letter in characters.reversed() {
+    print(letter, terminator: "")
 }
-} else {
-print("입력이 없습니다.")
-}
+
