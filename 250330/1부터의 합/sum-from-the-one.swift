39f6dcd9d223bf954dad99818efbe3bd4b576040
GthingkingG <21thingking@gmail.com>
2025-03-30 22:31:30 +0900

    Title: 1부터의 합, Time: 10ms, Memory: 1MB, Status: Passed - Codetree

--- 250330/1부터의 합/sum-from-the-one.swift
+++ 250330/1부터의 합/sum-from-the-one.swift
@@ -3,7 +3,7 @@ if let input = readLine() {
     var count = 0
     for i in 1...100 {
         count += i
-        if count > input {
+        if count >= input {
             print(i)
             break
         }
